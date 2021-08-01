a65ebf0d5d8bbd51b53597e3e7eaaeb10ba3c229
T.N <85272779+tatsuoNakano@users.noreply.github.com>
2021-08-02 07:40:54 +0900

    final end

--- src/pages/stockphoto.js
+++ src/pages/stockphoto.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -9,20 +8,23 @@ import "./style.scss"
 const Stockphoto = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.gdsgfdgfdgdg  ow go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <h1>Adobe stock</h1>
+    <p>コントリビューターポートフォリオページ</p>
+    <a href="https://stock.adobe.com/jp/contributor/209774197/%E9%81%94%E9%9B%84%20%E4%B8%AD%E9%87%8E" target="_blank" rel="noopener noreferrer nofollow">
+      <div>
+        <StaticImage
+          src="../images/st.jpg"
+          width={350}
+          quality={95}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="A Gatsby astronaut"
+          style={{ marginBottom: `1.45rem` }}
+        />
+      </div>
+    </a>
+    <p>↑バナーリンクです。外部サイトへ飛びます。</p>
+    <p>その他、Shutterstock・PIXTA・gettyimages等会社に有料写真素材を提供しております。</p>
+    <p>現在は、写真素材の販売数を増やす事を目標にしつつ、Adobe Illustratorのスキルアップに励み、いずれかにはベクター形式のイラスト素材を販売できるよう研鑽しております。</p>
   </Layout>
 )
 
